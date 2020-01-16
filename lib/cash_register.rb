64471775e4cd17243dea964c93f94064b90b778f
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-16 19:42:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,4 +9,9 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price*quantity
   end
+  
+  def apply_discount 
+    @total = @total - @total*(discount*1.0)/100
+    "Success the total is now #{@total}!"
+  end
 end
